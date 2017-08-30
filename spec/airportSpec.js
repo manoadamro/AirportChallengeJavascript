84334aa7ef80d099cc7ce45e69ba4bb4286b7fc6
Adam Romano <manoadamro.imp@gmail.com>
2017-08-30 15:37:28 +0100

    added tests for weather generation

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -18,9 +18,35 @@ describe("Airport", function() {
     expect(airport.hangar).not.toContain("Concorde 671");
   });
 
+  // it ("generates weather randomly", function () {
+  //   var clear = 0
+  //   var stormy = 0
+  //   for (var i = 0; i < 100; i++) {
+  //     weather = Weather.generate()
+  //     if (weather == 'stormy') {
+  //       stormy++
+  //     }
+  //     else {
+  //       clear++
+  //     }
+  //   }
+  //   expect(stormy).toBeGreaterThan(30)
+  //   expect(clear).toBeGreaterThan(30)
+  // })
 
+  it("should not take off during stormy weather", function () {
+    expect(airport.takeoff("Spitfire mk2", false)).toEqual("Spitfire mk2 could not take off.")
+  });
 
+  it("should take off during clear weather", function () {
+    expect(airport.takeoff("Spitfire mk2", true)).toEqual("Spitfire mk2 has taken off.")
+  });
 
+  it("should not land during stormy weather", function () {
+    expect(airport.land("Spitfire mk2", false)).toEqual("Spitfire mk2 could not land.")
+  });
 
-
+  it("should land during clear weather", function () {
+    expect(airport.land("Spitfire mk2", true)).toEqual("Spitfire mk2 has landed.")
+  });
 });
--- src/airport.js
+++ src/airport.js
@@ -2,17 +2,23 @@ function Airport(){
   this.hangar = []
 }
 
-Airport.prototype.land = function(plane) {
-  this.hangar.push(plane)
-  console.log(this.hangar)
-  return plane + " has landed.";
+Airport.prototype.land = function(plane, clear = true) {
+  if (clear) {
+    this.hangar.push(plane)
+    console.log(this.hangar)
+    return plane + " has landed.";
+  }
+  return plane + " could not land."
 };
 
-Airport.prototype.takeoff = function(plane) {
-  var planeIndex = this.hangar.indexOf(plane);
-// The indexOf method returns -1 if the value to search for never occurs."
-  if(planeIndex !== -1) {
-      this.hangar.splice(planeIndex, 1);
+Airport.prototype.takeoff = function(plane, clear = true) {
+  if (clear) {
+    var planeIndex = this.hangar.indexOf(plane);
+  // The indexOf method returns -1 if the value to search for never occurs."
+    if(planeIndex !== -1) {
+        this.hangar.splice(planeIndex, 1);
+    }
+    return plane + " has taken off.";
   }
-  return plane + " has taken off.";
+  return plane + " could not take off."
 };
