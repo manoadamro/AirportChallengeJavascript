b0e7d451026eab0ac0e8b77385800b8c8f7cfdb0
Adam Romano <manoadamro.imp@gmail.com>
2017-08-30 16:33:20 +0100

    added capacity (default and overridable). cleaned up tests

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -4,52 +4,58 @@ describe("Airport", function() {
         var isClear = true;
     });
 
-  it("should land a plane", function() {
-    expect(airport.land("Concorde 671", isClear)).toEqual("Concorde 671 has landed.");
-  });
+  describe ("Take off and Land", function () {
+    it("should land a plane", function() {
+      expect(airport.land("Concorde 671", isClear)).toEqual("Concorde 671 has landed.");
+    });
 
-  it("should be able to takeoff", function() {
-    expect(airport.takeoff("Concorde 671", isClear)).toEqual("Concorde 671 has taken off.");
-  });
+    it("should be able to takeoff", function() {
+      expect(airport.takeoff("Concorde 671", isClear)).toEqual("Concorde 671 has taken off.");
+    });
 
-  it("should confirm that a plane is no longer n the airport", function() {
-    airport.land("Concorde 671", isClear);
-    expect(airport.hangar).toContain("Concorde 671");
-    airport.takeoff("Concorde 671", isClear);
-    expect(airport.hangar).not.toContain("Concorde 671");
-  });
+    it("should confirm that a plane is no longer n the airport", function() {
+      airport.land("Concorde 671", isClear);
+      expect(airport.hangar).toContain("Concorde 671");
+      airport.takeoff("Concorde 671", isClear);
+      expect(airport.hangar).not.toContain("Concorde 671");
+    });
+  })
 
-  // it ("generates weather randomly", function () {
-  //   var clear = 0
-  //   var stormy = 0
-  //   for (var i = 0; i < 100; i++) {
-  //     weather = Weather.generate()
-  //     if (weather == 'stormy') {
-  //       stormy++
-  //     }
-  //     else {
-  //       clear++
-  //     }
-  //   }
-  //   expect(stormy).toBeGreaterThan(30)
-  //   expect(clear).toBeGreaterThan(30)
-  // })
+  describe ("Weather", function () {
+    it("should not take off during stormy weather", function () {
+      var isClear = false
+      expect(airport.takeoff("Spitfire mk2", isClear)).toEqual("Spitfire mk2 could not take off.")
+    });
 
-  it("should not take off during stormy weather", function () {
-    var isClear = false
-    expect(airport.takeoff("Spitfire mk2", isClear)).toEqual("Spitfire mk2 could not take off.")
-  });
+    it("should take off during clear weather", function () {
+      expect(airport.takeoff("Spitfire mk2", isClear)).toEqual("Spitfire mk2 has taken off.")
+    });
 
-  it("should take off during clear weather", function () {
-    expect(airport.takeoff("Spitfire mk2", isClear)).toEqual("Spitfire mk2 has taken off.")
-  });
+    it("should not land during stormy weather", function () {
+      var isClear = false
+      expect(airport.land("Spitfire mk2", isClear)).toEqual("Spitfire mk2 could not land.")
+    });
 
-  it("should not land during stormy weather", function () {
-    var isClear = false
-    expect(airport.land("Spitfire mk2", isClear)).toEqual("Spitfire mk2 could not land.")
-  });
+    it("should land during clear weather", function () {
+      expect(airport.land("Spitfire mk2", isClear)).toEqual("Spitfire mk2 has landed.")
+    });
+  })
 
-  it("should land during clear weather", function () {
-    expect(airport.land("Spitfire mk2", isClear)).toEqual("Spitfire mk2 has landed.")
-  });
+  describe ("Capacity", function () {
+    it ("should not exceed capacity", function () {
+      for (var i = 0; i < airport.capacity; i++) {
+        airport.land("Plane " + i, true)
+      }
+      expect(airport.land("Plane X", true)).toEqual("Airport full!")
+    })
+
+    it ("should have a default capacity of " + DEFAULT_AIRPORT_CAPACITY, function () {
+      expect(airport.capacity).toEqual(DEFAULT_AIRPORT_CAPACITY)
+    })
+
+    it ("should allow capacity to be overridden", function () {
+      airport = new Airport(10);
+      expect(airport.capacity).toEqual(10)
+    })
+  })
 });
--- src/airport.js
+++ src/airport.js
@@ -1,8 +1,15 @@
-function Airport(){
+
+DEFAULT_AIRPORT_CAPACITY = 5
+
+function Airport(capacity = DEFAULT_AIRPORT_CAPACITY){
   this.hangar = []
+  this.capacity = capacity
 }
 
 Airport.prototype.land = function(plane, clear = isClear()) {
+  if (this.hangar.length >= this.capacity) {
+    return "Airport full!"
+  }
   if (clear) {
     this.hangar.push(plane)
     console.log(this.hangar)
